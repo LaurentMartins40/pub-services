8ca4da83b428abf310c273424f4b7a4e8198bd9b
laurent <laurentmartins@outlook.com>
2019-03-22 16:11:12 +0100

    pub services es6

--- pub-services/services/pub.service.js
+++ pub-services/services/pub.service.js
@@ -1,9 +1,45 @@
 const pubs = require('../mocks/pub.json');
 
-function findAll(){
-    return pubs.map(item => item.name);
+
+
+
+class Owner{
+    constructor(firstName,lastName,mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mail = mail;
+    }
+}
+class Beers {
+    constructor(type,name){
+        this.type = type;
+        this.name = name;
+    }
+}
+class OpenHours{
+    constructor(start,end){
+        this.start = start;
+        this.end = end;
+    }
+}
+class Pubs {
+    constructor(name,owner,openDays,openHours,beers){
+        this.name = name;
+        this.owner = owner;
+        this.openDays = openDays;
+        this.openHours = openHours;
+        this.beers = beers;
+    }
 }
 
-module.exports= {
-    findAll:findAll,
-} 
+function findAll(){
+    return pubs.map(p=> new Pubs(p.name, 
+        new Owner(p.owner.firstName,p.owner.lastName,p.owner.mail),
+        p.openDays, 
+        new OpenHours(p.openHours.start,p.openHours.end),p.beers.map(b=> new Beers(b.type,b.name))
+        ))
+    }
+
+    module.exports= {
+        findAll:findAll,
+    } 
